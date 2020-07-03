7087ba9bba48fc8f756f420b8231dd925d038c44
S.Pragashraj <rajpragashraj30@gmail.com>
2020-07-03 10:49:00 +0530

    firebase config

---
+++ src/config/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+
+const config={
+    apiKey: "AIzaSyBbjg9FzUO-jOC4TzCSkmFjkYOdj4ILnlc",
+    authDomain: "the-recipe-281912.firebaseapp.com",
+    databaseURL: "https://the-recipe-281912.firebaseio.com",
+    projectId: "the-recipe-281912",
+    storageBucket: "the-recipe-281912.appspot.com",
+    messagingSenderId: "446129426639",
+    appId: "1:446129426639:web:7baa67ea3e0d5dd4aa67ba",
+    measurementId: "G-97E3RFQ0MQ"
+}
+
+firebase.initializeApp(config)
+
+export const fbase=firebase
+export const database=firebase.database()
+export const auth=firebase.auth()
+export const storage=firebase.storage()
